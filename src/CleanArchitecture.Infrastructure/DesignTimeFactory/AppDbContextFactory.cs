ae401fb424acbe4aba61c7d55be9df7744efab38
Arsh <arshdeepsingh22@gmail.com>
2024-11-10 22:36:10 +1100

    wip

---
+++ src/CleanArchitecture.Infrastructure/DesignTimeFactory/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArchitecture.Infrastructure.DesignTimeFactory;
+
+public class AppDbContextFactory :IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Configure the DbContextOptions with the connection string
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
